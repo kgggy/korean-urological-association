e14528294b4e6aa5a342dc1efb3764a567aba75c
kgggy <douwn77@naver.com>
2022-03-14 14:32:35 +0900

    first commit!!

--- config/db.js
+++ config/db.js
@@ -5,37 +5,8 @@ const conn = { // mysql 접속 설정
     port: '3306',
     user: 'resoft',
     password: 'Qwert1234!',
-    database: 'carbon',
+    database: 'urological_association_mmate',
     multipleStatements: true
 };
 
-// const connt = async () => {
-//     try {
-//         var connection = mysql.createConnection(conn); // DB 커넥션 생성
-//         connection.connect();
-//     } catch(error) {
-//         console.log(error);
-//     }
-// }
-// DB 접속
-
-// var testQuery = "INSERT INTO `members` (`username`,`password`) VALUES ('test','test');";
-
-// connection.query(testQuery, function (err, results, fields) { // testQuery 실행
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(results);
-// });
-
-// var testQuery = "SELECT * FROM user";
-
-// connection.query(testQuery, function (err, results, fields) { // testQuery 실행
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(results);
-// });
-
 module.exports = conn;
-//connection.end(); // DB 접속 종료
