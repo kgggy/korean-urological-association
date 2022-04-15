7c679594779047c8f1dfaa0d8e142a29dc8f7ecb
suu <9l0522@naver.com>
2022-04-15 13:54:26 +0900

    paging

--- routes/api/event.js
+++ routes/api/event.js
@@ -63,25 +63,5 @@ router.get('/voteList', async (req, res) => {
     }
 });
 
-//투표 명단
-router.get('/voteTest', async (req, res) => {
-    try {
-        const sql = "call checkvote(14,@yes, @nono, @undefine);\
-                        select @yes, @nono, @undefine; "
-        const param = req.query.eventId;
-        connection.query(sql, (err, result) => {
-            if (err) {
-                console.log(err);
-                res.json({
-                    msg: "query error"
-                });
-            }
-            console.log(result[1]);
-            res.status(200).json(result);
-        });
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
 
 module.exports = router;
